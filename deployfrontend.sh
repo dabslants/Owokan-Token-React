859a1d88224e888684ca0f6e1962de338a83b9be
qp <dabslant@gmail.com>
2019-06-10 14:27:59 +0100

    Compile assets for GitHub pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,5 +1,5 @@
-npm run build
-rsync build/static/* docs/
+#npm run build
+rsync build/* docs/
 git add .
 git commit -m "Compile assets for GitHub pages"
 git push -u origin master
---
+++ docs/precache-manifest.efa2a42822c62d937b0208a258f16ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ba70eb360165bccde40bc81004f76b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65105f249fb50385f17",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "590a8bcd7cebce02d058",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "d65105f249fb50385f17",
+    "url": "/static/js/2.4188754c.chunk.js"
+  },
+  {
+    "revision": "590a8bcd7cebce02d058",
+    "url": "/static/js/main.957fa661.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
