c45cc805d893cdb737220ef5c112165ce7c0a4e0
qp <dabslant@gmail.com>
2019-06-10 15:01:25 +0100

    Updates

---
+++ precache-manifest.17bdfc200a3c6db8be28a627dbb7be95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba521020911edd1d55cbe26ce216fc8",
+    "url": "/Owokan-Token-React/index.html"
+  },
+  {
+    "revision": "a7d6359ca8ca87188ce3",
+    "url": "/Owokan-Token-React/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "170b3c0f5ed2e2604f58",
+    "url": "/Owokan-Token-React/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "a7d6359ca8ca87188ce3",
+    "url": "/Owokan-Token-React/static/js/2.7b5b27f5.chunk.js"
+  },
+  {
+    "revision": "170b3c0f5ed2e2604f58",
+    "url": "/Owokan-Token-React/static/js/main.0f838bd0.chunk.js"
+  },
+  {
+    "revision": "e5bf0356d59b9ebf0faf",
+    "url": "/Owokan-Token-React/static/js/runtime~main.09fedced.js"
+  }
+]);
